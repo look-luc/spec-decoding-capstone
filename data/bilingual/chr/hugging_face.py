60bb29ce8837bff5f731633dc0b90b4c2275ce4c
Luc De Nardi <162731427+look-luc@users.noreply.github.com>
2026-01-25 19:49:27 -0700

    Mono bi corpus (#8)
    * starting to add monolingual data
    
    * starting to add monolingual data
    
    * starting to add monolingual and cherokee bilingual data
    
    * starting to add monolingual data
    
    * starting to add monolingual data
    
    * starting to add monolingual data
    
    * finished adding monolingual data
    
    * reworking tatoeba for some langs
    
    * adding new igbo and occitan data
    
    * reworking igbo dat
    
    * finished monoling reference table and data
    
    * starting retriving data py
    
    * continuing trying to run python path + changing all huggingface files
    
    * continuing trying to run python path
    
    * continuing trying to run python path
    
    * continuing trying to run python path
    
    * continuing trying to run python path
    
    * finished working on getting data

---
+++ data/bilingual/chr/hugging_face.py
@@ -0,0 +1,5 @@
+from datasets import load_dataset
+
+
+def load_data():
+    return load_dataset("wang4067/cherokee-english-45")
---
+++ data/monolingual/zgh/hugging_face.py
@@ -0,0 +1,5 @@
+from datasets import load_dataset
+
+
+def load_data():
+    return load_dataset("Tamazight-NLP/IRCAM-CORPUS")
